bfa5a68058ef0f73120feaf1c225350899930b0b
Aung Htet Nyein <aunghtetnyein1999@gmail.com>
2022-03-25 09:37:32 +0630

    :tada: initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,9 +3,9 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `new`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-react-helmet"]
+  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-postcss"],
 };
 
 export default config;
